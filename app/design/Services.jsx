b26a25a253525fb0eb06f1c7a6678d911068d0fa
M0mahmoud <houdmohamed85@gmail.com>
2024-04-08 04:09:45 +0200

    Adding Services Section

--- app/design/Services.jsx
+++ app/design/Services.jsx
@@ -1,12 +1,12 @@
-import { brainwaveWhiteSymbol, gradient, play } from "../assets";
-import ChatBubbleWing from "../assets/svg/ChatBubbleWing";
+import { gradient, play } from "../assets";
+import ChatBubbleWing from "./svg/ChatBubbleWing";
 
 export const Gradient = () => {
   return (
     <div className="absolute top-0 -left-[10rem] w-[56.625rem] h-[56.625rem] opacity-50 mix-blend-color-dodge pointer-events-none">
       <img
         className="absolute top-1/2 left-1/2 w-[79.5625rem] max-w-[79.5625rem] h-[88.5625rem] -translate-x-1/2 -translate-y-1/2"
-        src={gradient}
+        src={gradient.src}
         width={1417}
         height={1417}
         alt="Gradient"
@@ -24,34 +24,11 @@ export const PhotChatMessage = () => {
   );
 };
 
-export const VideoChatMessage = () => {
-  return (
-    <div className="absolute top-8 left-[3.125rem] w-full max-w-[14rem] pt-2.5 pr-2.5 pb-7 pl-5 bg-n-6 rounded-t-xl rounded-br-xl font-code text-base md:max-w-[17.5rem]">
-      Video generated!
-      <div className="absolute left-5 -bottom-[1.125rem] flex items-center justify-center w-[2.25rem] h-[2.25rem] bg-color-1 rounded-[0.75rem]">
-        <img
-          src={brainwaveWhiteSymbol}
-          width={26}
-          height={26}
-          alt="Brainwave"
-        />
-      </div>
-      <p className="tagline absolute right-2.5 bottom-1 text-[0.625rem] text-n-3 uppercase">
-        just now
-      </p>
-      <ChatBubbleWing
-        className="absolute bottom-0 right-full -scale-x-100"
-        pathClassName="fill-n-6"
-      />
-    </div>
-  );
-};
-
 export const VideoBar = () => {
   return (
     <div className="absolute bottom-0 left-0 flex items-center w-full p-6">
       <img
-        src={play}
+        src={play.src}
         width={24}
         height={24}
         alt="Play"
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Benefits from "./(HomeSections)/Benefits";
 import Collaboration from "./(HomeSections)/Collaboration";
+import Services from "./(HomeSections)/Services";
 import Hero from "./(HomeSections)/hero";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Benefits />
       <Collaboration />
+      <Services />
     </div>
   );
 }
